8c15024df443cd547c01d871ef49836a2e3813a9
bohdaholas <bohdan.ivashko@ucu.edu.ua>
2021-04-12 10:55:18 +0300

    Reading patient's data via csv module

--- examples/patients_data_csv_reader.py
+++ examples/patients_data_csv_reader.py
@@ -1,5 +1,17 @@
+import csv
 
 
+class Patient:
+    def __init__(self, patient_data):
+        self.patient_data = patient_data
 
 
-
+with open("diabetes_registry.csv") as file:
+    patients_data = list(csv.reader(file))
+    obtained_patient_data = patients_data[0]
+    patients = []
+    for patient in patients_data:
+        patient_data = {}
+        for idx, value in enumerate(patient):
+            patient_data[obtained_patient_data[idx]] = value
+        patients.append(Patient(patient_data))
